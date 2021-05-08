231d9a5615dd19fb5b61aa445a6a611828c55dd1
unknown <andrey.talivanov@gmail.com>
2021-05-08 16:40:43 +0200

    created squares

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import Square, { NamesProps } from './Square';
 
 function App() {
+  const names: Array<NamesProps> = [
+    {
+      name: "erwe",
+      color: 'red',
+    },
+    {
+      name: "erw2e",
+      color: 'yellow',
+    },
+    {
+      name: "erwe3",
+      color: 'red',
+    },
+    {
+      name: "erwe4",
+      color: 'yellow',
+    }
+];
+
   return (
     <div className="App">
-      
+      {names.map(({ color, name }) => 
+        <Square isRed={color === "red"} name={name} />
+      )}
     </div>
   );
 }
